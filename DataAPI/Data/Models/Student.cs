2174491ada4989814a7ddb0aae9488d162228e07
qutr14 <lequoctrung1407@gmail.com>
2022-12-20 22:47:31 +0700

    create, get and delete student

---
+++ DataAPI/Data/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿namespace DataAPI.Data.Models
+{
+    public class Student
+    {
+        public string RollNumber { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string BirthDate { get; set; }
+        public string ImageName { get; set; }
+        public IFormFile Image { get; set; }
+    }
+
+    public class StudentToGet
+    {
+        public string RollNumber { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string BirthDate { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class StudentToDelete
+    {
+        public string RollNumber { get; set; }
+    }
+}
---
+++ Symphony/Areas/Admin/Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Symphony.Areas.Admin.Models
+{
+    public class StudentModel
+    {
+        public string RollNumber { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string BirthDate { get; set; }
+        public string ImageName { get; set; }
+    }
+}
