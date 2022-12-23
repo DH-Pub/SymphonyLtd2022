1f8009b9d39cd2841505b6b649f4897b4a9b7d13
qutr14 <lequoctrung1407@gmail.com>
2022-12-23 13:02:09 +0700

    teacher create, delete, detai and update

---
+++ DataAPI/Data/Models/Teacher.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAPI.Data.Models
+{
+    public class Teacher
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string BirthDate { get; set; }
+
+    }
+
+    public class TeacherId
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ Symphony/Areas/Admin/Models/TeacherModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Symphony.Areas.Admin.Models
+{
+    public class TeacherModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string BirthDate { get; set; }
+    }
+}
