8a86b98f86050b5735b9793bf4ef27d87e285a2a
qutr14 <lequoctrung1407@gmail.com>
2022-12-30 15:44:35 +0700

    Update UI

--- Symphony/Areas/Student/Controllers/CourseDetailController.cs
+++ Symphony/Areas/Student/Controllers/CourseDetailController.cs
@@ -1,28 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Symphony.Areas.Admin.Models;
 using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
 using System.Text.Json;
 
 namespace Symphony.Areas.Student.Controllers
 {
     [Area("student")]
-    public class CourseController : Controller
+    public class CourseDetailController : Controller
     {
         private const string _tokenName = "token";
-        public IActionResult Index()
-        {
-            HttpClient httpClient = new HttpClient();
-            var res = httpClient.GetAsync(Program.ApiAddress + "api/Course/GetAllCourses").Result;
-            var data = res.Content.ReadAsStringAsync().Result;
-            CourseListApi result = JsonSerializer.Deserialize<CourseListApi>(data);
-            if (result.Status)
-            {
-                return View(result.Data);
-            }
-            return View(new List<CourseListApi>());
-        }
-
-        public IActionResult Detail(string id)
+        public IActionResult Index(string id)
         {
             string token = Request.Cookies[_tokenName];
             HttpClient httpClient = new HttpClient();
@@ -34,7 +24,7 @@ namespace Symphony.Areas.Student.Controllers
             {
                 return View(result.Data);
             }
-            return RedirectToAction("Index");
+            return RedirectToAction("Home/Index");
         }
     }
 }
